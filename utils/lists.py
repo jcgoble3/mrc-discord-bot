8fc739191f9d8b3ba5f8b03135b14fc0e998b65c
Jonathan Goble <jcgoble3@gmail.com>
2021-03-17 21:23:27 -0400

    Demonstrate QuestionAnswer and QuestionList (#25)
    Basic framework of the actual question list to provide something for
    others to make pull requests to and to show them how to write it.
    
    Also adjusted indentation to match other code and silence linter.

--- utils/lists.py
+++ utils/lists.py
@@ -1,6 +1,15 @@
+from utils.trivia_data import QuestionAnswer, QuestionList
+
 ballresponse = [
-  'Yes', 'No', 'Take a wild guess...', 'Very doubtful',
-  'Sure', 'Without a doubt', 'Most likely', 'Might be possible',
-  "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
-  'senpai, pls no ;-;'
+    'Yes', 'No', 'Take a wild guess...', 'Very doubtful',
+    'Sure', 'Without a doubt', 'Most likely', 'Might be possible',
+    "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
+    'senpai, pls no ;-;'
 ]
+
+_all_questions = [
+    QuestionAnswer("What is the capital city of Spain?", "Madrid"),
+    QuestionAnswer("What is arachnophobia the fear of?", "Spiders"),
+]
+
+trivia_questions = QuestionList(_all_questions)
