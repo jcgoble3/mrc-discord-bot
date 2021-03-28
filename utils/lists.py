e65931dea4fdee5f1923f2afc8480c9337939063
Seth Sarchet <seth_sarchet@outlook.com>
2021-03-28 18:45:29 -0400

    Contributed trivia questions and answers into the code as part of Task #29

--- utils/lists.py
+++ utils/lists.py
@@ -10,6 +10,22 @@ ballresponse = [
 _all_questions = [
     QuestionAnswer("What is the capital city of Spain?", "Madrid"),
     QuestionAnswer("What is arachnophobia the fear of?", "Spiders"),
+    QuestionAnswer("Stockholm is the capital and largest city of what country?", "Sweden"),
+    QuestionAnswer("When did the website “Facebook” launch?", "2004"),
+    QuestionAnswer("T/F: The scientific name for the Southern Lights is Aurora Australis?", "T"),
+    QuestionAnswer("On average, Americans consume 100 pounds of what per second?", "Chocolate"),
+    QuestionAnswer("How many bones are there in the human body?", "206"),
+    QuestionAnswer("What does Na stand for on the periodic table?", "Sodium"),
+    QuestionAnswer("Which fictional city is the home of Batman?", "Gotham"),
+    QuestionAnswer("Which planet is closest to Earth?", "Venus"),
+    QuestionAnswer("In the US version of The Office, what is the name of the city they live in?", "Scranton"),
+    QuestionAnswer("Which U.S. State is the largest?", "Alaska"),
+    QuestionAnswer("Which U.S. State is known as the “Sunflower State”?", "Kansas"),
+    QuestionAnswer("Who founded Microsoft?", "Bill Gates"),
+    QuestionAnswer("What plant is known to help heal a sunburn?", "Aloe"),
+    QuestionAnswer("What is the #1 cookie in the United States?", "Oreo"),
+    QuestionAnswer("How many planets are in our solar system? (Including Pluto!)", "9"),    
+
 ]
 
 trivia_questions = QuestionList(_all_questions)
