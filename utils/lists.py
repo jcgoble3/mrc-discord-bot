009869806a8ac9dd14c0adf8d83774665ce033ed
Max Lubbers <ferreri51@gmail.com>
2021-03-30 20:13:02 -0400

    Update lists.py

--- utils/lists.py
+++ utils/lists.py
@@ -10,6 +10,7 @@ ballresponse = [
 _all_questions = [
     QuestionAnswer("What is the capital city of Spain?", "Madrid"),
     QuestionAnswer("What is arachnophobia the fear of?", "Spiders"),
+<<<<<<< Updated upstream
     QuestionAnswer("What is your body’s largest organ?", "Skin"),
     QuestionAnswer("What year did the Titanic movie come out?", "1997"),
     QuestionAnswer("Who played Neo in The Matrix?", "Keanu Reeves"),
@@ -17,6 +18,24 @@ _all_questions = [
     QuestionAnswer("Which bone are babies born without?", "Knee Cap"),
     QuestionAnswer("What is Hawkeye's real Name?", "Clint Barton"),
     QuestionAnswer("Mexico’s Dia de los Muertos, means what in English?", "Day of the Dead"),
+=======
+    QuestionAnswer("Stockholm is the capital and largest city of what country?", "Sweden"),
+    QuestionAnswer("What year did the website Facebook launch?", "2004"),
+    QuestionAnswer("What is the scientific name for the Southern Lights?", "Aurora Australis"),
+    QuestionAnswer("On average, Americans consume 100 pounds of what per second?", "Chocolate"),
+    QuestionAnswer("How many bones are there in the human body?", "206"),
+    QuestionAnswer("What does Na stand for on the periodic table?", "Sodium"),
+    QuestionAnswer("Which fictional city is the home of Batman?", "Gotham"),
+    QuestionAnswer("Which planet is closest to Earth?", "Venus"),
+    QuestionAnswer("In the US version of The Office, what is the name of the city they live in?", "Scranton"),
+    QuestionAnswer("Which U.S. State is the largest (in terms of land area)?", "Alaska"),
+    QuestionAnswer("Which U.S. State is known as the Sunflower State?", "Kansas"),
+    QuestionAnswer("Who founded Microsoft?", "Bill Gates"),
+    QuestionAnswer("What plant is known to help heal a sunburn?", "Aloe"),
+    QuestionAnswer("How many planets are in our solar system? (Including Pluto!)", "9"),  
+      
+
+>>>>>>> Stashed changes
 ]
 
 trivia_questions = QuestionList(_all_questions)
