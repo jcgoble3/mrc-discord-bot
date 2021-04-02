c480790bff7224d0912362597e64d81ad171244f
Seth Sarchet <seth_sarchet@outlook.com>
2021-04-02 17:29:07 -0400

    Added a file to store the questions and answers, then placed everything in the QuestionList

--- utils/lists.py
+++ utils/lists.py
@@ -10,7 +10,7 @@ i = 0
 answers = []
 questions = []
 
-with open ("/Users/sethsarchet/Desktop/QuestionList.txt") as fp:
+with open ("trivia_questions_answers.txt") as fp:
     count = 0
     a_count = 0
     q_count = 0
@@ -19,31 +19,15 @@ with open ("/Users/sethsarchet/Desktop/QuestionList.txt") as fp:
         count +=1
         if count % 2 == 0: # answer
             answers.append(line)
-            print(answers[a_count])
             a_count+=1
         else: # question
             questions.append(line)
-            print(questions[q_count])
             q_count+=1
-            
-_all_questions = [
-    QuestionAnswer("What is the capital city of Spain?", "Madrid"),
-    QuestionAnswer("What is arachnophobia the fear of?", "Spiders"),
-    QuestionAnswer("Stockholm is the capital and largest city of what country?", "Sweden"),
-    QuestionAnswer("What year did the website Facebook launch?", "2004"),
-    QuestionAnswer("What is the scientific name for the Southern Lights?", "Aurora Australis"),
-    QuestionAnswer("On average, Americans consume 100 pounds of what per second?", "Chocolate"),
-    QuestionAnswer("How many bones are there in the human body?", "206"),
-    QuestionAnswer("What does Na stand for on the periodic table?", "Sodium"),
-    QuestionAnswer("Which fictional city is the home of Batman?", "Gotham"),
-    QuestionAnswer("Which planet is closest to Earth?", "Venus"),
-    QuestionAnswer("In the US version of The Office, what is the name of the city they live in?", "Scranton"),
-    QuestionAnswer("Which U.S. State is the largest (in terms of land area)?", "Alaska"),
-    QuestionAnswer("Which U.S. State is known as the Sunflower State?", "Kansas"),
-    QuestionAnswer("Who founded Microsoft?", "Bill Gates"),
-    QuestionAnswer("What plant is known to help heal a sunburn?", "Aloe"),
-    QuestionAnswer("How many planets are in our solar system? (Including Pluto!)", "9"),    
 
-]
+
+_all_questions = []
+for i in range(q_count):
+     _all_questions.append(QuestionAnswer(questions[i], answers[i]))
+        
 
 trivia_questions = QuestionList(_all_questions)
