4bbd8a63bfbd11e1dc7fbd72d66927b85b585e4a
Joshua Luckenbill <joshua.luckenbill@centauricorp.com>
2021-04-22 19:08:22 -0400

    Implement programming by contract and literate programming

--- utils/trivia.py
+++ utils/trivia.py
@@ -78,31 +78,67 @@ class Trivia:
     inProgress: bool
 
     def __init__(self):
+        
+        if __debug__:
+            assert type(self) is Trivia, "self must be of type Trivia"
+            assert type(self.qlist) is QuestionList, "The qlist variable must be of type QuestionList"
+            assert type(qAsked) is QuestionAnswer, "The qAsked variable must be of type QuestionAnswer"
+
+        # This line prepopulates the question list and is only here to facilitate
+        # ease of development. Had development continued far enough, this line would
+        # be replaced with code that would read in the questions and answers from an
+        # external source.
         self.qlist = QuestionList([QuestionAnswer("Question 1", "Answer 1")])
+
+        if __debug__:
+            assert type(self.inProgress) is bool, "The inProgress variable must be of type boolean"
+
         self.inProgress = False
+        
+        if __debug__:
+            assert self.inProgress == False, "inProgress must be False when the Trivia object is initialized"
 
-    # This function is responsible for starting the trivia game.
-    def start(self) -> int:
-        # Return non-zero value to indicate failure
-        # Return 0 to indicate success
+    ## This function is responsible for starting the trivia game.
+    # Return non-zero value to indicate failure
+    # Return 0 to indicate success
+    def start_game(self) -> int:
+        if __debug__:
+            assert type(self) is Trivia, "self must be of type Trivia"
+
         return 0
 
-    # This function is responsible for ending the trivia game.
-    def end(self) -> int:
-        # Return non-zero value to indicate failure
-        # Return 0 to indicate success
+    ## This function is responsible for ending the trivia game.
+    # Return non-zero value to indicate failure
+    # Return 0 to indicate success
+    def end_game(self) -> int:
+        if __debug__:
+            assert type(self) is Trivia, "self must be of type Trivia"
+
         return 0
 
-    def check(self, answer) -> int:
-        # Return non-zero value to indicate failure
-        # Return 0 to indicate success
+    ## This function is responsible for checking submitted answers
+    # to see if they are correct or not.
+    # Return non-zero value to indicate failure
+    # Return 0 to indicate success
+    def check_answer(self, answer) -> int:
+        if __debug__:
+            assert type(self) is Trivia, "self must be of type Trivia"
+            assert type(answer) is str, "The answer argument must be of type str"
+
         if (self.qAsked.answer == answer):
             return 0
         else:
             return -1
         
 
-    # This function is responsible for returning the status of the
+    ## This function is responsible for returning the status of the
     # trivia game.
-    def status(self) -> str:
-        return "Trivia Status!"
+    def get_game_status(self) -> str:
+        if __debug__:
+            assert type(self) is Trivia, "self must be of type Trivia"
+            assert type(self.inProgress) is bool, "inProgress must be of type bool"
+
+        if (self.inProgress):
+            return "Trivia game in progress"
+        else:
+            return "Trivia game is not in progress"
