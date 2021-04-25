5d730bcecb6bf81d8a15544bd6e5f82377006f64
Jonathan Goble <jcgoble3@gmail.com>
2021-04-25 16:03:45 -0400

    Pack question loading into reusable function
    This enables the function to be tested as part of #71.

--- utils/lists.py
+++ utils/lists.py
@@ -7,33 +7,33 @@ ballresponse = [
     "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
     'senpai, pls no ;-;'
 ]
-i = 0
-answers = []
-questions = []
-
-## @story{9} The bot reads in a list of questions to choose from.
-with open ("trivia_questions_answers.txt") as fp:
-    count = 0
-    a_count = 0
-    q_count = 0
-    
-    for line in fp:
-        count +=1
-        if count % 2 == 0: # answer
-            answers.append(line)
-            a_count+=1
-        else: # question
-            questions.append(line)
-            q_count+=1
 
 
-_all_questions = []
-for i in range(q_count):
-     _all_questions.append(QuestionAnswer(questions[i], answers[i]))
+## @story{9} The bot needs a list of questions to choose from.
+# @param filename name of file to load the questions and answers from
+def load_trivia_questions(filename: str):
+    answers = []
+    questions = []
+
+    with open(filename) as fp:
+        count = 0
+
+        for line in fp:
+            count += 1
+            if count % 2 == 0:  # answer
+                answers.append(line)
+            else:  # question
+                questions.append(line)
+
+    all_questions = []
+    for i in range(len(questions)):
+        all_questions.append(QuestionAnswer(questions[i], answers[i]))
+    return all_questions
 
 ## @story{9} The list of questions needs to be packed into the designed
 #  data structure.
-trivia_questions = QuestionList(_all_questions)
+trivia_questions = QuestionList(
+    load_trivia_questions("trivia_questions_answers.txt"))
 
 #List for holding file names from meme directory
 all_memes = []
